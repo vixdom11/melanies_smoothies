5e923c8addd90d5557ddc2e631ca98f5bde60291
vixdom11 <117307358+vixdom11@users.noreply.github.com>
2024-04-11 10:00:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +  ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
     #st.write(ingredients_string)
 
@@ -50,7 +53,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
