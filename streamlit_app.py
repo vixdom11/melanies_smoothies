60c1ac43a7bed2123520a4e766b7d44276fbcb96
vixdom11 <117307358+vixdom11@users.noreply.github.com>
2024-04-11 10:34:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Custumize Your Smoothie! :cup_with_straw:")
