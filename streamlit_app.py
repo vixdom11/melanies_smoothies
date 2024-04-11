3fa39e11adc037ce43399242839c71a730b3d9a6
vixdom11 <117307358+vixdom11@users.noreply.github.com>
2024-04-11 09:52:50 +0200

    Update streamlit_app.py
    Add library request

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Custumize Your Smoothie! :cup_with_straw:")
@@ -47,4 +48,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
